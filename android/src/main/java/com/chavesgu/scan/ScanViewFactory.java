e3011555230404a0bde90168b9d7a3fdb5d002ef
datt16 <osatom.4639@gmail.com>
2023-03-07 00:04:00 +0900

    Examining the context problem solving method
    検証1 contextを再生成してみる
    https://github.com/flutter/engine/pull/35997/files

--- android/src/main/java/com/chavesgu/scan/ScanViewFactory.java
+++ android/src/main/java/com/chavesgu/scan/ScanViewFactory.java
@@ -2,6 +2,7 @@ package com.chavesgu.scan;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.ContextWrapper;
 
 import java.util.Map;
 
@@ -18,19 +19,28 @@ public class ScanViewFactory extends PlatformViewFactory {
     @NonNull private final BinaryMessenger messenger;
     @NonNull private final Context context;
     @NonNull private final Activity activity;
+    private final boolean preserveContext;
     private ActivityPluginBinding activityPluginBinding;
 
-    ScanViewFactory(@NonNull BinaryMessenger messenger, @NonNull Context context, @NonNull Activity activity, @NonNull ActivityPluginBinding activityPluginBinding) {
+    ScanViewFactory(@NonNull BinaryMessenger messenger, @NonNull Context context, @NonNull Activity activity, @NonNull ActivityPluginBinding activityPluginBinding, boolean preserveContext) {
         super(StandardMessageCodec.INSTANCE);
         this.messenger = messenger;
         this.context = context;
         this.activity = activity;
         this.activityPluginBinding = activityPluginBinding;
+        this.preserveContext = preserveContext;
     }
 
+    @NonNull
     @Override
     public PlatformView create(Context context, int viewId, Object args) {
         final Map<String, Object> creationParams = (Map<String, Object>) args;
-        return new ScanPlatformView(messenger, this.context, this.activity, this.activityPluginBinding, viewId, creationParams);
+
+        if(preserveContext) {
+            return new ScanPlatformView(messenger, this.context, this.activity, this.activityPluginBinding, viewId, creationParams);
+        } else {
+            final Context differentContext = new ContextWrapper(context);
+            return new ScanPlatformView(messenger, differentContext, this.activity, this.activityPluginBinding, viewId, creationParams);
+        }
     }
 }
